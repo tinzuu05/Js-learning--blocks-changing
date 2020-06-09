3ab58bec86bfb46d6710828e464fcf7b7d70d458
tinzuu05 <64735248+tinzuu05@users.noreply.github.com>
2020-06-09 06:13:28 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,15 @@
+function toGreen()
+{
+	div2.style.width='300px'
+	div2.style.height='300px'
+	div2.style.background='green'
+}
+
+
+function toRed()
+{
+	div2.style.width='200px'
+	div2.style.height='200px'
+	div2.style.background='red'
+}
+
